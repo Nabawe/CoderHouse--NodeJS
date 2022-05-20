80d09ad5df90961f4178644b1cc18439d64b2c44
Nabawe <Nabawe@users.noreply.github.com>
2022-05-20 00:53:56 -0300

    Fixed runTest, missed () to run the cb, the need to wrap in () => the testees and reconfirmed the need of await to handle async funcs returns

--- 02__Node_FS/index.mjs
+++ 02__Node_FS/index.mjs
@@ -6,7 +6,7 @@ console.log( `==============================${ new Date() }` );
 
 function runTest( msg, delay, test ) {
     setTimeout(
-        ( async () => { console.log( msg, await test, '\n' ) } ),
+        ( async () => { console.log( msg, await test(), '\n' ) } ),
         delay
     );
 };
@@ -16,13 +16,13 @@ function runTest( msg, delay, test ) {
     ! La class esta armada para solo usar el archivo si esta en ./testFiles/ el mismo directorio, ya q la consigna decia q se le pasara un nombre
 */
 let file = new Container( 'productos.json' );
-runTest( 'deleteAll: ', 70, file.deleteAll() );
+runTest( 'deleteAll: ', 70, () => file.deleteAll() );
 /* Si el delay del intervalo es lo suficientemente bajo se lo puede hacer explotar */
 let i = 0;
 const interID = setInterval(
     () => {
         if ( i < TestObjects.length ) {
-            runTest( 'Saving... ', null, file.save( TestObjects[i++] ) );
+            runTest( 'Saving... ', null, () => file.save( TestObjects[i++] ) );
         } else {
             clearInterval(interID);
         }
@@ -30,30 +30,43 @@ const interID = setInterval(
     150
 );
 
-runTest( 'getById hit: ', 5000, file.getById( 3 ) );
-runTest( 'getById miss: ', 6000, file.getById( '3' ) );
-runTest( 'getLastId: ', 7000, file.getLastId() );
+runTest( 'getById hit: ', 3000, () => file.getById( 3 ) );
+runTest( 'getById miss: ', 4000, () => file.getById( '3' ) );
+runTest( 'getLastId: ', 5000, () => file.getLastId() );
 /*
-runTest( 'Save: ', null, file.save( TestObjects[0] ) ); // Para causar errores correr 3 o 5 simultaneos.
-runTest( 'Save: ', null, file.save( TestObjects[4] ) ); // Para causar errores correr 3 o 5 simultaneos.
-runTest( 'Save: ', null, file.save( TestObjects[4] ) ); // Para causar errores correr 3 o 5 simultaneos.
-runTest( 'Save: ', null, file.save( TestObjects[4] ) ); // Para causar errores correr 3 o 5 simultaneos.
-runTest( 'Save: ', null, file.save( TestObjects[2] ) ); // Para causar errores correr 3 o 5 simultaneos.
-runTest( 'Save: ', null, file.save( TestObjects[1] ) ); // Para causar errores correr 3 o 5 simultaneos.
-runTest( 'Save: ', null, file.save( TestObjects[1] ) ); // Para causar errores correr 3 o 5 simultaneos.
-runTest( 'Save: ', null, file.save( TestObjects[6] ) ); // Para causar errores correr 3 o 5 simultaneos.
-runTest( 'Save: ', null, file.save( TestObjects[6] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[0] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[4] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[4] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[4] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[2] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[1] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[1] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[6] ) ); // Para causar errores correr 3 o 5 simultaneos.
+runTest( 'Save: ', null, () => file.save( TestObjects[6] ) ); // Para causar errores correr 3 o 5 simultaneos.
 */
 
-runTest( 'deleteById miss: ', 8000, file.deleteById( 500 ) );
-runTest( 'deleteById hit: ', 9000, file.deleteById( 3 ) );
-runTest( 'deleteById hit; reviwing changes to the Array: ', 10000, file.getAll() );
+runTest( 'deleteById miss: ', 6000, () => file.deleteById( 500 ) );
+runTest( 'deleteById hit: ', 7000, () => file.deleteById( 3 ) );
+runTest( 'Reviwing changes to the Array after delition: ', 8000, () => file.getAll() );
 
 
 // ( async () => { console.log( 'MANUAL TEST ', await file.getById( 4 ), '\n' ) } )();
-( async () => { console.log( 'MANUAL TEST ', await file.getAll(), '\n' ) } )();
+// ( async () => { console.log( 'MANUAL TEST ', await file.getAll(), '\n' ) } )();
 
 // ! EmptyFile y EmptyJSON
+console.log( `==============================${ new Date() }` );
+file = new Container( 'EmptyFile.json' );
+runTest( 'getById miss: ', 4000, () => file.getById( '3' ) );
+runTest( 'deleteById miss: ', 6000, () => file.deleteById( 500 ) );
 
+
+console.log( `==============================${ new Date() }` );
+file = new Container( 'EmptyFileJSON.json' );
+runTest( 'deleteAll: ', 70, () => file.deleteAll() );
+runTest( 'getById miss: ', 4000, () => file.getById( '3' ) );
+runTest( 'deleteById miss: ', 6000, () => file.deleteById( 500 ) );
+
+
+// Testeando fs.readdirSync
 // import fs from 'fs';
 // console.log( fs.readdirSync('./testFiles/') );
